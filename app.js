2b613e80568861fdc20636a9775c5dcc2b4c81ed
Sudip Mahato <mahatosudip837@gmail.com>
2023-04-21 02:11:48 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,17 @@
+const box =document.querySelector('#box')
+
+box.addEventListener('mouseover',()=>{
+    box.classList.remove('active')
+    console.log(box);
+})
+
+const openMenu = document.querySelector('.menu');
+const closeMenu = document.querySelector('.close');
+const navbar = document.querySelector('nav ul');
+
+openMenu.addEventListener('click',()=>{
+    navbar.classList.add('active')
+})
+closeMenu.addEventListener('click',()=>{
+    navbar.classList.remove('active')
+})
